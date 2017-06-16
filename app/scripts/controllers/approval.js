8588d65e01098ee901d0f02d0de780af4d95575c
yehong you <youyehong@gmail.com>
2017-06-16 21:07:32 +1000

    time line function

--- app/scripts/controllers/approval.js
+++ app/scripts/controllers/approval.js
@@ -15,22 +15,38 @@ antloans.controller('approvalCtrl',['$scope','jobService','$stateParams',
              $('.job_edit').show();
         });
 
+        $('.cancelBtn01').click(function(){
+          $('.job_show').show();
+          $('.job_edit').hide();
+        });
+
         $('.saveBtn01').click(function(){
           $('.job_show').show();
           $('.job_edit').hide();
         });
 
       // ========= edit vertical timeline ========
-      console.log(Array.isArray(status));
+      var status = [];
+      status.push($('.circle.1'),$('.circle.2'),$('.circle.3'),$('.circle.4'),$('.circle.5'),
+                  $('.circle.6'),$('.circle.7'),$('.circle.8'),$('.circle.9'),$('.circle.10'),
+                  $('.circle.11'),$('.circle.12'));
+
        $('.timeline_editBtn').click(function(){
           $('.timeline').css('border-left',"3px dotted #B5B5B5");
           $('.circle').css('cursor','pointer');
           $('.circle').click(function(e){
              $(e.target).toggleClass('done');
-             $(e.target).prev().addClass('done');
-            //  console.log(status.indexOf($(e.target)));
+             var index = parseInt($(e.target).attr('data-index'));
+             if($(e.target).hasClass("done")){
+                for(var i = 0; i<index; i++){
+                  status[i].addClass('done')
+                }
+             }else{
+               for(var j = index+1; j<=status.length+1; j++){
+                  status[j].removeClass('done');
+               }
+             }
           });
-
 
        })
 
